cf6741b8832771d48fdbd8ffaf5a17b5596ea62f
raixxxxxxxx6 <semkalox5@gmail.com>
2025-12-12 12:22:43 +0400

    создал MainActivity

---
+++ MainActivity.kt
@@ -0,0 +1,28 @@
+package com.campingbooking.app.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.campingbooking.app.R
+import com.campingbooking.app.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+}
+
